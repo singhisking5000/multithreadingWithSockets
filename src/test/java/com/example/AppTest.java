90d842279a02e9c4d85ac84a92fad05aa2fe3fe5
Fedor Menchukov <77518360+menchukf@users.noreply.github.com>
2026-03-13 11:13:30 -0400

    Initial commit

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        assertFalse( false );
+    }
+}
